78ce392ecf3ba19d09d5d026271b7729a4671e5e
Tushar Mistry <tusharmistry@MacBook-Air.local>
2020-12-03 16:25:09 +0530

    Added some styling

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import { data } from "./data.js";
 import { renderMantriCard } from "./components/mantriDiv.js";
 
 window.onload = () => {
-  const rootDiv = document.getElementById('root');
+  const mantriDiv = document.getElementById('mantri-card');
   data.then((mantris) => {
     mantris.forEach((mantri) => {
-      rootDiv.appendChild(renderMantriCard(mantri));
+      mantriDiv.appendChild(renderMantriCard(mantri));
     });
   }).catch(e => {
     console.warn(e);
--- src/data.js
+++ src/data.js
@@ -3,6 +3,6 @@ const url = 'https://tushar1998.github.io/json-data-server/mantriCards.json';
 // immediately invoked function IIFE
 export const data = (async () => {
     let response = await axios.get(url);
-    console.log(response.data);
+    // console.log(response.data);
     return response.data;
 })();
